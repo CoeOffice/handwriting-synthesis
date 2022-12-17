189123b8625e533ea8986083f36839674a31a949
CoeOffice <120782657+CoeOffice@users.noreply.github.com>
2022-12-17 16:25:54 +0530

    Update ct83_demo.py

--- ct83_demo.py
+++ ct83_demo.py
@@ -11,8 +11,11 @@ if __name__ == '__main__':
 
     # usage demo
     lines = [
-        "What is Lorem Ipsum",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        "FUNCTIONS",
+        "Definition : Let X and Y be nonempty sets. A function f from X to Y is
+an assignment of exactly one element of Y to each element of X. We write
+f (x) = y if y is the unique element of Y assigned by the function f to the
+element x of X. If f is a function from X to Y, we write f :X → Y",
         "Where does it come from?",
         "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. ",
         "What is Lorem Ipsum",
